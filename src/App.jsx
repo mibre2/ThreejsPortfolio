09c191532816589a68a7b55f2ad7e0235a161915
mibre2 <michael_breckenridge@ymail.com>
2025-05-20 03:19:39 -0400

    Navbar initial creation and installed react motion for animating the ui.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Navbar from "./sections/Navbar";
 
 const App = () => {
-  return <div className="text-3xl font-bold underline text-red-500">App</div>;
+  return <div className="text-3xl font-bold underline text-red-500"><Navbar /></div>;
 };
 
 export default App;
