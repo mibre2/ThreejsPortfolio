2bce62c9ddf9a7ab5cd27d3a4563bd830216bbc3
mibre2 <michael_breckenridge@ymail.com>
2025-05-23 18:19:36 -0400

    imported dummy data for later use in projects, experience, and testimonial sections. Included project section dummy data along with pop up modal functionality and animations.

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,5 +1,20 @@
+import { myProjects } from "../components/constants";
+import Project from "../components/Project";
+
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <section className="relative c-space section-spacing">
+      <h2 className="text-heading">My Selected Projects</h2>
+      <div
+        className="bg-gradient-to-r from-transparent via-neutral-700 
+      to-transparent mt-12 h-[1px] w-full"
+      />
+      {myProjects.map((project) => (
+        <Project key={project.id} {...project} />
+      ))}
+      Projects
+    </section>
+  );
 };
 
 export default Projects;
