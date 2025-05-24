acbd0e7eaba751c0f135a1be5baa66a7425c76bb
mibre2 <michael_breckenridge@ymail.com>
2025-05-24 01:01:46 -0400

    Completed Project section.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,10 +1,22 @@
 import ProjectDetails from "./ProjectDetails";
 import { useState } from "react";
-const Project = ({ title, description, subDescription, href, image, tags }) => {
+const Project = ({
+  title,
+  description,
+  subDescription,
+  href,
+  image,
+  tags,
+  setPreview
+}) => {
   const [isHidden, setIsHidden] = useState(false);
   return (
     <>
-      <div className="flex-wrap items-center justify-between py-10 space-y-14 sm:flex sm:space-y-0">
+      <div
+        className="flex-wrap items-center justify-between py-10 space-y-14 sm:flex sm:space-y-0"
+        onMouseEnter={() => setPreview(image)}
+        onMouseLeave={() => setPreview(null)}
+      >
         <div>
           <p className="tex-2xl">{title}</p>
           <div className="flex gap-5 mt-2 text-sand">
