f35deef3591923e10b1fe75c6e133a5034491149
mibre2 <michael_breckenridge@ymail.com>
2025-05-29 22:39:24 -0400

    Initial Commit for contact section.

---
+++ src/sections/Contact.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <section className="relative flex items-center c-space section-spacing">
+      Contact Section
+    </section>
+  );
+};
+
+export default Contact;
