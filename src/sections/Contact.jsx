3c8827aa86a4351aa0c70cdee050be1e4dc548da
mibre2 <michael_breckenridge@ymail.com>
2025-05-30 16:43:49 -0400

    Added Full Name, Email, Message, and submit sections/button styles to Contact section.

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,7 +1,65 @@
 const Contact = () => {
   return (
-    <section className="text-heading relative flex items-center c-space section-spacing">
-      Contact Section
+    <section className="relative flex items-center c-space section-spacing">
+      <div className="flex flex-col items-center justify-center max-w-md p-5 mx-auto border border-white/10 rounded-2xl bg-primary">
+        <div className="flex flex-col items-start w-full gap-5 mb-10">
+          <h2 className="text-heading">Let's Connect</h2>
+          <p className="font-normal text-neutral-400">
+            Whether you're looking to build something new or collaborate on an
+            existing project, I'm always open to new opportunities. Let's chat!
+          </p>
+        </div>
+        <form className="w-full">
+          <div className="mb-5">
+            <label htmlFor="name" className="field-label">
+              Full Name
+            </label>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              className="field-input field-input-focus"
+              placeholder="John Doe"
+              autoComplete="name"
+              required
+            />
+          </div>
+
+          <div className="mb-5">
+            <label htmlFor="email" className="field-label">
+              Email
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              className="field-input field-input-focus"
+              placeholder="JohnDoe@email.com"
+              autoComplete="email"
+              required
+            />
+          </div>
+
+          <div className="mb-5">
+            <label htmlFor="Message" className="field-label">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              type="message"
+              rows="4"
+              className="field-input field-input-focus mb-10"
+              placeholder="Share your thoughts..."
+              autoComplete="message"
+              required
+            />
+          </div>
+          <button className="w-full px-1 py-3 text-lg text-center rounded-md cursor-pointer bg-radial from-lavender to-royal hover-animation">
+            Submit
+          </button>
+        </form>
+      </div>
     </section>
   );
 };
