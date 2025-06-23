24b2ce0611d40ea2a747bb698756c064e62a9a88
mibre2 <michael_breckenridge@ymail.com>
2025-06-23 12:51:37 -0400

    added usestates to contact section with event handlers.

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   return (
     <section className="relative flex items-center c-space section-spacing">
       <div className="flex flex-col items-center justify-center max-w-md p-5 mx-auto border border-white/10 rounded-2xl bg-primary">
@@ -21,6 +32,8 @@ const Contact = () => {
               className="field-input field-input-focus"
               placeholder="John Doe"
               autoComplete="name"
+              value={formData.name}
+              onChange={handleChange}
               required
             />
           </div>
@@ -36,22 +49,26 @@ const Contact = () => {
               className="field-input field-input-focus"
               placeholder="JohnDoe@email.com"
               autoComplete="email"
+              value={formData.email}
+              onChange={handleChange}
               required
             />
           </div>
 
           <div className="mb-5">
-            <label htmlFor="Message" className="field-label">
+            <label htmlFor="message" className="field-label">
               Message
             </label>
             <textarea
               id="message"
               name="message"
-              type="message"
+              type="text"
               rows="4"
               className="field-input field-input-focus mb-10"
               placeholder="Share your thoughts..."
               autoComplete="message"
+              value={formData.message}
+              onChange={handleChange}
               required
             />
           </div>
