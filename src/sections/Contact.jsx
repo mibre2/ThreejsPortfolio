b71e9d5454d80eaf8cb0e7aa170b2d878a7cd4d7
mibre2 <michael_breckenridge@ymail.com>
2025-07-02 14:08:35 -0400

    Included particle effect to Contact section.

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -59,7 +59,13 @@ const Contact = () => {
 
   return (
     <section className="relative flex items-center c-space section-spacing">
-      <Particles />
+      <Particles
+        className="absolute inset-0 -z-50"
+        quantity={100}
+        ease={80}
+        color={"#ffffff"}
+        refresh
+      />
       <div className="flex flex-col items-center justify-center max-w-md p-5 mx-auto border border-white/10 rounded-2xl bg-primary">
         <div className="flex flex-col items-start w-full gap-5 mb-10">
           <h2 className="text-heading">Let's Connect</h2>
