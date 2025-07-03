620b4ddfd54f9e2436a5fb76438e2464d3787bce
mibre2 <michael_breckenridge@ymail.com>
2025-07-02 21:41:03 -0400

    Fixed case sensitive import

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import Card from "../components/card";
+import { Card } from "../components/Card";
 import { Globe } from "../components/Globe";
 import CopyEmailButton from "../components/CopyEmailButton";
 import { Frameworks } from "../components/Frameworks";
